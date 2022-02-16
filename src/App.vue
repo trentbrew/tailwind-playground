1fbc754f09beadbc1be72bbe245d5fd07be90cfd
tbrew1023 <tbrew1023@gmail.com>
2022-02-16 11:59:39 -0600

    setup tailwind

--- src/App.vue
+++ src/App.vue
@@ -16,14 +16,3 @@ console.log(store);
   <button @click="store.increment(1)">Increment</button>
   <button @click="store.waitAndAdd">Wait + Increment</button>
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
